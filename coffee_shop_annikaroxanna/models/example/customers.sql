7e1741706fc3889f787e831775f84c92e6570450
annikaroxanna <118449194+annikaroxanna@users.noreply.github.com>
2023-06-02 14:47:25 +0000

    deleting model examples

--- coffee_shop_annikaroxanna/models/example/customers.sql
+++ coffee_shop_annikaroxanna/models/example/customers.sql
@@ -1,19 +1,23 @@
-with orders AS (
-  select 
-    customer_id
-    , min(created_at) as first_order_at
-    , count(*) as number_of_orders
-  from `analytics-engineers-club.coffee_shop.orders`
-  group by 1
+{{ config(
+    materialized='table'
+) }}
+
+with customer_orders as (
+    select
+        customer_id,
+        count(*) as number_of_orders,
+        min(created_at) as first_order_at
+    from `analytics-engineers-club.coffee_shop.orders`
+    group by 1
 )
 
 select
-  customers.id as customer_id
-  , customers.name 
-  , customers.email
-  , orders.first_order_at
-  , orders.number_of_orders
+    customers.id as customer_id,
+    customers.name,
+    customers.email,
+    customer_orders.first_order_at,
+    customer_orders.number_of_orders
+
 from `analytics-engineers-club.coffee_shop.customers` as customers
-left join orders
-on customers.id =  orders.customer_id
-order by orders.first_order_at
+left join customer_orders
+    on customers.id = customer_orders.customer_id
