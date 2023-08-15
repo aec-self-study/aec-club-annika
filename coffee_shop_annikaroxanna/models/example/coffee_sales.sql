fbb3d4b53dc18c0c51f93f181c642be987e20a38
annikaroxanna <118449194+annikaroxanna@users.noreply.github.com>
2023-08-15 12:01:17 +0000

    adding sales sql

---
+++ coffee_shop_annikaroxanna/models/example/coffee_sales.sql
@@ -0,0 +1,12 @@
+{{ config(
+  materialized = 'table'
+  )}}
+
+select
+  date_trunc(sold_at, month) as date_month,
+  {%- for product_category in ['coffee_beans', 'merch', 'brewing_supplies']}
+    sum(case when product_category = '{{product_category}}' then total end) as {{product_category}}_amount
+  {%- if not loop.last %}, {% endif -%}
+  {% endfor %}
+from {{ ref('sales') }}
+group by 1
